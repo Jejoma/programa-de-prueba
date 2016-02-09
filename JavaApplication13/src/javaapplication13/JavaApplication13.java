fc6275372e98719bca551b706184d74faf2a4349
jejom_000 <jejom_000@User>
2016-02-08 20:05:47 -0500

    3 version

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -8,11 +8,13 @@ public class JavaApplication13 {
     public static void main(String[] args) {
        
     
-    int j = 5+5;
-    int suma=j;
-    System.out.println("la suma es : "+suma);
+    
+    
     
     }    
             
+       public void metodo(){
+           System.out.println("" + "");
            
+       }    
 }
